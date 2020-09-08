3db366f88649b7a264d4c789548f471c1bcad67c
Sugestor <16436376+Sugestor@users.noreply.github.com>
2020-09-08 19:38:23 +0200

    lauffähig

--- FPShooter.py
+++ FPShooter.py
@@ -4,16 +4,7 @@ import random
 import math
 import logging
 
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    K_SPACE,
-    KEYDOWN,
-    QUIT,
-)
+from pygame.locals import K_ESCAPE, K_SPACE, KEYDOWN, QUIT
 
 BLACK = ( 0, 0, 0)
 GRAY = ( 100, 100, 100)
@@ -33,120 +24,156 @@ screen = pygame.display.set_mode(SIZE)
 HARD_PUNISH = 500 #1000
 SOFT_PUNISH = 250
 
+WAIT_AFTER_HIT_DURATION = 250
+WAIT_BEFORE_START_DURATION = 255
+SHRINKING_SPEED = 180 # More is faster
+
+TARGET_RADIUS = 400
+HIT_RADIUS = 8
+
+HIT_DURATION = 85
 
 
+
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.target = Target()
+        
+    def add_to_score(self, points):
+        self.score += points
+        
+
+    def update(self):
+        self.score += self.target.update()
+
+        name_text = font.render("{}".format(self.name), True, BLACK, WHITE)
+        screen.blit(name_text, [10, 10])
+       
+        score_text = font.render("Punkte: {}".format(self.score), True, BLACK, WHITE)
+        screen.blit(score_text, [10, SIZE[1]-20])
+
+
+    def create_bullet_hole(self, x, y):
+        self.score += self.target.create_bullet_hole(x, y)
+
+
+        
+    
+    
 
 class Target():
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.score = 0
-        self.radius = 400
-        self.speed = 180
-        self.stop_duration = 250
-        self.stop = False
-        self.hits = []
+        self.counter = 0
         self.new_target()
-        
+
 
     def new_target(self):
         self.x = random.randint(int(SIZE[0]*0.1), int(SIZE[0]*0.9))
         self.y = random.randint(int(SIZE[1]*0.1), int(SIZE[1]*0.9))
-        logging.debug(self.x, self.y)
-        self.radius = 400
         self.target_new = True
-        self.target_new_time = 255
+        self.radius = TARGET_RADIUS
+        self.wait_after_hit = False
+        self.wait_after_hit_duration = WAIT_AFTER_HIT_DURATION
+        self.target_new_time = WAIT_BEFORE_START_DURATION
         self.color = RED
-        
+
 
     def update(self):
         if self.target_new: # neues Ziel, etwas warten
-            self.target_new_time -= 1
-            self.color = (self.target_new_time, 0, 0) #change color from red to black
-            if self.target_new_time <= 1:
+            if self.target_new_time > 0:
+                self.target_new_time -= 1
+                self.color = (self.target_new_time, 0, 0) #change color from red to black
+            else:
                 self.target_new = False
                 self.color = BLACK
         else:                
-            if self.stop: # stop after hit / new target after time
-                self.stop_duration -= 1
-                if self.stop_duration <= 1:
-                    self.stop = False
-                    self.stop_duration = 150
+            if self.wait_after_hit: # stop after hit / new target after time
+                if self.wait_after_hit_duration > 0:
+                    self.wait_after_hit_duration -= 1
+                else:
+                    self.counter += 1
                     self.new_target()
-
             else:
-                self.radius -= self.radius / self.speed
-                if self.radius < 8:
-                    self.score += HARD_PUNISH
+                if self.radius > 8: 
+                    self.radius -= (self.radius / SHRINKING_SPEED)
+                else: # target too small
+                    self.counter += 1
                     self.new_target()
+                    return HARD_PUNISH
 
                 
         # draw target first
         pygame.draw.circle(screen, self.color, (self.x,self.y), int(self.radius), 0)
-
-        # then draw all hits
-        for hit in self.hits:
-            if hit.duration <= 1:
-                self.hits.remove(hit)
-            else:
-                hit.update()
-
-        score_text = font.render("Punkte: {}".format(self.score), True, BLACK, WHITE)
+        
         current_radius = font.render("Radius: {:.0f}".format(self.radius), True, BLACK, WHITE)
-        screen.blit(score_text, [10, SIZE[1]-20])
         screen.blit(current_radius, [10, SIZE[1]-45])
         
+        return 0
+
 
     def create_bullet_hole(self, x, y):
         if self.target_new: # hit before start
-            self.score += HARD_PUNISH
-            self.hits.append(Bullet_Hole(x, y, RED))
-        elif self.stop: # nach einem Treffer
-            self.score += HARD_PUNISH
-            self.hits.append(Bullet_Hole(x, y, RED))
+            hits.append(Bullet_Hole(x, y, RED, HARD_PUNISH))
+            return HARD_PUNISH
+        elif self.wait_after_hit: # nach einem Treffer
+            hits.append(Bullet_Hole(x, y, RED, HARD_PUNISH))
+            return HARD_PUNISH
         else:
             # calculate if hit  
             if math.sqrt((self.x - x)**2 + ( self.y - y)**2) <= self.radius:
-                self.score += int(self.radius)
-                logging.info("treffer {}".format(self.radius))
-                logging.info("self.x {}, self.y {}, x {}, y {}".format(self.x, self.y, x, y))
-                self.hits.append(Bullet_Hole(x, y, GREEN))
-                self.stop=True
+                hits.append(Bullet_Hole(x, y, GREEN, int(self.radius)))
+                self.wait_after_hit = True
+                return int(self.radius)
             else: # Miss
-                self.score += SOFT_PUNISH
-                self.hits.append(Bullet_Hole(x, y, RED))
+                hits.append(Bullet_Hole(x, y, RED, SOFT_PUNISH))
+                return SOFT_PUNISH
+        return 0
             
 
 
 class Bullet_Hole():
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color, score):
         self.x = x
         self.y = y
-        self.duration = 50
+        self.radius = HIT_RADIUS
+        self.duration =  HIT_DURATION
         self.color = color
+        self.score = score
+        self.score_position = 0
 
     def update(self):
+        if self.duration < HIT_RADIUS:
+            self.radius = self.duration 
+        pygame.draw.circle(screen, self.color, (self.x,self.y), int(self.radius), 0)
+        score_text = font.render("{}".format(self.score), True, self.color)
+        screen.blit(score_text, [self.x + 10, self.y - 10 + int(self.score_position)])
         self.duration -= 1
-        hit_radius = self.duration / 2
-        pygame.draw.circle(screen, self.color, (self.x,self.y), int(hit_radius), 0)
+        self.score_position -= 0.5
 
 
+hits=[]
+players = []
+#all_sprite_list = pygame.sprite.Group()
+#bullet_hole_list = pygame.sprite.Group()
 
-
-
+players.append(Player("a"))
+players.append(Player("b"))
+players.append(Player("c"))
         
 def main():
-
     pygame.init()
-
     pygame.display.set_caption("FPShooter")
     clock = pygame.time.Clock()
     
-    target = Target()
-
+    current_player = 0
 
     running = True
     display_instructions = True
+    gameover = False
     
     while running and display_instructions and False:
         for event in pygame.event.get():
@@ -161,22 +188,31 @@ def main():
         instruction_text = font.render("Instructions", True, WHITE)
         screen.blit(instruction_text, [10, 10])
 
-
-            # Limit to 60 frames per second
         clock.tick(20)
-     
-        # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
 
         
-    while running:
+    while running and not gameover:
 
         screen.fill( WHITE )
         pygame.draw.line(screen, GRAY, (int(SIZE[0]/2), 0), (int(SIZE[0]/2), int(SIZE[1])))
         pygame.draw.line(screen, GRAY, (0, int(SIZE[1]/2)), (int(SIZE[0]), int(SIZE[1]/2)))
 
-        target.update()
-        
+        if players[current_player].target.counter >= 2:
+            current_player += 1
+            if current_player >= len(players):
+                running = False
+        else:
+            players[current_player].update()
+
+        # then draw all hits
+        for hit in hits:
+            if hit.duration > 0:
+                hit.update()
+            else:
+                hits.remove(hit)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -186,12 +222,34 @@ def main():
                 if event.key == K_SPACE:
                     x = random.randint(SIZE[0]*0.2, SIZE[0]*0.8)
                     y = random.randint(SIZE[0]*0.2, SIZE[0]*0.8)
-                    target.create_bullet_hole(x, y)
-
+                    players[current_player].create_bullet_hole(x, y)
 
 
         clock.tick(100)
         pygame.display.flip()
+
+
+        
+    while not gameover and False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameover = True
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    gameover = True
+                    
+        screen.fill( GRAY )
+
+        offset = 0
+        
+        for player in players:
+            score_text = font.render("{}: {}".format(player.name, player.score), True, BLACK)
+            screen.blit(score_text, [10, 10 + offset])
+            offset += 25
+
+        clock.tick(20)
+        pygame.display.flip()
+        
 
     pygame.quit()
 
